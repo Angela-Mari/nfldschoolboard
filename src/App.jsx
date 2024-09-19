f57950e4117d3a3b6bbe455788ece275ce10dc0f
Angela George <ageorge5@zagmail.gonzaga.edu>
2024-09-18 19:41:20 -0500

    new links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Card from './Card';
 import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
 import Col from 'react-bootstrap/Col';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const candidates = {
@@ -10,19 +13,22 @@ function App() {
         name: "Claudia Gonzalez-George",
         bio: "I’ve been in the field of education most of my adult life either teaching or supporting students.  As a first-generation American and proud parent of three, I believe in public education. I’m grateful for the opportunity I’ve had during my term on the school board to impact Northfield students at the policy and governance level, and hope to continue serving.",
         photo: '/assets/Claudia.jpg',
-        fblink: "https://www.facebook.com/claudiaforschoolboard/"
+        fblink: "https://www.facebook.com/claudiaforschoolboard/",
+        sbquestionlink: "https://drive.google.com/file/d/1LKrQPpVZ8FxV5Sdx4x3yTBAnyMgYXOm1/view?fbclid=IwY2xjawFV0DVleHRuA2FlbQIxMAABHdE3DQw9S2E2SnXeirGh_99H4i5BQGxC1v_YN4yuA7Tu5xWy9-JYqU3yOg_aem_ZiJFbfP-CIUdB2-UJPuz5g"
     },
     candidate2: {
       name: "Corey Butler",
       bio: "I'm the father of two students in the district, the husband of a townie, and the son of a school bus driver and custodian. I deeply care about the power of public schools and want to use my experience in life, work, and my first term on the school board to continue to serve our community and guide our remarkable district into the future.",
       photo: '/assets/corey.jpg',
-      fblink: "https://www.facebook.com/CoreyforNorthfieldSchools"
+      fblink: "https://www.facebook.com/CoreyforNorthfieldSchools",
+      sbquestionlink:"https://drive.google.com/file/d/1dUTSmo3Rrph_P-oah1o-dimWGDnIEzlt/view?usp=sharingCorey"
   },
   candidate3: {
     name: "Maggie Epstein",
     bio: "As a parent, foster parent, and engaged member of the Northfield community, I have developed a deep commitment to our schools and a desire to serve on the school board. I have educational and professional experience in the field of education and believe that public schools are the key to a thriving democracy.",
     photo: '/assets/maggie.jpg',
-    fblink: "https://www.facebook.com/maggieepsteinschoolboard"
+    fblink: "https://www.facebook.com/maggieepsteinschoolboard",
+    sbquestionlink:"https://drive.google.com/file/d/1ZYC597DXumuHLwXu2a3cyam0xrJxoOcs/view"
 }
 
 }
@@ -33,15 +39,29 @@ function App() {
      <Navbar style={{backgroundColor:"#7A0019", justifyContent:"center"}}>
     <h1 style={{textAlign:"center"}}>United for Northfield</h1>
     </Navbar>
-    <div style={{padding:"2rem"}}>
+    
    
-    <Row>    
+    
 
-    </Row>
+
+    <div style={{padding:"1rem"}}>
+   
+    <Accordion defaultActiveKey="0">    
+      <Accordion.Item eventKey='0'>
+        <Accordion.Header style={{textAlign:"center"}}><img width="30" height="30" src="https://img.icons8.com/ios-filled/50/warranty.png" alt="warranty"/><b>Endorsed by Northfield Education Association</b></Accordion.Header>
+        <Accordion.Body>
+          <p>
+          NEA endorsed Claudia Gonzalez-George, Maggie Epstein, Corey Butler, and Amy Goerwitz based on their experience, interviews, and questionaires.
+          </p>
+          <Button href="https://www.facebook.com/northfieldeducationassociation/">Learn More</Button>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    
     <Row>
       {
       Object.entries(candidates).map(([key, value]) => (
-        <Card key ={key} name={value.name} bio={value.bio} photo={value.photo} fblink={value.fblink}></Card>
+        <Card key ={key} name={value.name} bio={value.bio} photo={value.photo} fblink={value.fblink} sbquestionlink={value.sbquestionlink}></Card>
       ))}
     </Row>
 
@@ -73,13 +93,34 @@ function App() {
         <Row style={{paddingLeft:"1rem"}}>
         <a href="https://northfieldschools.org/reimagine/">What Are We Doing About the High School?</a>
         </Row>
+        <Row style={{paddingLeft:"1rem"}}>
+        <a href="https://kymnradio.net/2024/09/14/meet-the-candidates-for-northfield-school-board-2/ ">KYMN elections guide</a>
+        </Row>
+        <Row style={{paddingLeft:"1rem"}}>
+        <a href="https://lwvnorthfieldmn.org/2024/08/05/event-9-21-24-candidate-forums/
+">League of Women Voters of Northfield Cannon Falls: Nonpartisan Candidate Forum</a>
+        <p style={{paddingLeft:"1rem"}}>
+          Date: September 21, 2024 <br/>
+          Time: 1:00-2:00PM (for the Northfield School Board) <br/>
+          Location: Viking Theater, Buntrock Commons, St. Olaf College (1521 Campus Dr)<br/>
+          The forums will be recorded and available to <a href="https://www.stolaf.edu/multimedia/play/?e=4865">digitally stream</a> and will also be posted on the LWVNCF YouTube page and website in early October.
+
+        </p>
+        </Row>
+        <Row style={{paddingLeft:"1rem"}}>
+          Northfield News elections guide (coming in October)
+        </Row>
       </Col>
 
 
     </Row>
     
     </div>
-    <footer style={{backgroundColor:"#7A0019", textAlign:"center", padding:".5rem"}}> <p style={{color:"#FFCC33"}}> Designed by <a style={{color:"#FFCC33"}} href="https://angelageorge.com">Angela George</a> © 2024 </p></footer>
+    <footer style={{backgroundColor:"#7A0019", textAlign:"center", padding:".5rem"}}> 
+      <p style={{color:"#FFFFFF"}}>Prepared and paid for by the candidates</p>
+      <p style={{color:"#FFFFFF"}}>P.O. Box 13 <br /> Northfield, MN 55057</p>
+      <p style={{color:"#FFCC33"}}> Designed by <a style={{color:"#FFCC33"}} href="https://angelageorge.com">Angela George</a> © 2024 </p>
+    </footer>
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,9 @@ import './Card.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Card = ({name, photo, bio, fblink}) => {
+const Card = ({name, photo, bio, fblink, sbquestionlink}) => {
   
     console.log("Photo prop:", photo)
   return (
@@ -14,18 +15,23 @@ const Card = ({name, photo, bio, fblink}) => {
             <h3>{name}</h3>
         </Row>
         <Row style={{paddingBottom:".5rem"}}>
-                    <img width="25" height="25" src="https://img.icons8.com/ios-glyphs/30/facebook-new.png" alt="facebook-new"/>
-                    <a href={fblink}>More on Facebook</a>
-                </Row>
+            <div>
+            <img height={25} width={25} src="https://img.icons8.com/ios-glyphs/30/facebook-new.png" alt="facebook-new"/>
+            <a href={fblink}>More on Facebook</a>
+            </div>
+        </Row>
+        <Row style={{paddingBottom:".5rem"}}><a href={sbquestionlink}> NEA School Board Candidate Questionnaire</a></Row>
         <Row>
                
-            <Col xs={12} sm={4} lg={3}>
+            <Col xs={12} sm={4} lg={4}>
 
                 <Row style={{marginBottom:"1rem"}}>
+                    <div>
                     <img src={photo} alt={name} className='candidate-photo'/>
+                    </div>
                 </Row>
             </Col>
-            <Col xs={12} sm={8} lg={9} >
+            <Col xs={12} sm={8} lg={8} >
                 {bio}
             </Col>
         </Row>
